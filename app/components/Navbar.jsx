402471629cb2778dc1761487c573fcbad243a2db
Sebastian Zgheyb <sebastian1.zgheyb@gmail.com>
2025-02-17 13:35:04 +1100

    Adding dark mode

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { useState, useEffect, useRef } from "react";
 
 
-const Navbar = () => {
+const Navbar = ({isDarkMode, setIsDarkMode}) => {
 
     const [isScroll, setIsScroll] = useState(false);
     const sideMenuRef = useRef();
@@ -28,14 +28,14 @@ const Navbar = () => {
 
   return (
     <>
-    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden'>
         <Image src={assets.header_bg_color} alt="" className='w-full'/>
     </div>
-      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backgroup-blur-lg shadow-sm" : ""}`}>
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backgroup-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : ""}`}>
         <a href="#top">
-            <Image src={assets.logo} alt="Logo" className="w-36 cursor-pointer mr-14" />
+            <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt="Logo" className="w-36 cursor-pointer mr-14" />
         </a>
-        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : " bg-white shadow-sm bg-opacity-50"}`} >
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : " bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"}`} >
             <li><a href="#top" className='font-Ovo'>Home</a></li>
             <li><a href="#about" className='font-Ovo'>About Me</a></li>
             <li><a href="#services" className='font-Ovo'>Services</a></li>
@@ -43,16 +43,16 @@ const Navbar = () => {
             <li><a href="#contact" className='font-Ovo'>Contact me</a></li>
         </ul> 
         <div className='flex items-center gap-4'>
-            <button>
-                <Image src={assets.moon_icon} alt="" className='w-6'/>
+            <button onClick={()=> setIsDarkMode(prev => !prev)}>
+                <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt="" className='w-6'/>
             </button>
-            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image src={assets.arrow_icon} alt="" className='w-3'/></a>
-            <button className='block md:hidden ml-3' onClick={openMenu}><Image src={assets.menu_black} alt="" className='w-6'/></button>
+            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50'>Contact <Image src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon} alt="" className='w-3'/></a>
+            <button className='block md:hidden ml-3' onClick={openMenu}><Image src={isDarkMode ? assets.menu_white : assets.menu_black} alt="" className='w-6'/></button>
         </div>
         {/* -- ---- mobile menu ---- -- */}
-        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white'>
             <div className='absolute right-6 top-6' onClick={closeMenu}> 
-                <Image src={assets.close_black} alt='' className='w-5 cursor-pointer' />
+                <Image src={isDarkMode ? assets.close_white : assets.close_black} alt='' className='w-5 cursor-pointer' />
             </div>
             <li><a href="#top" className='font-Ovo' onClick={closeMenu}>Home</a></li>
             <li><a href="#about" className='font-Ovo' onClick={closeMenu}>About Me</a></li>
--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const ovo = Ovo({
 });
 
 export const metadata = {
-  title: "Seb's Portfolio",
+  title: "Sebastian's Portfolio",
   description: "",
 };
 
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
         {children}
       </body>
